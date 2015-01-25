01e8d0e7e49857f194e0b7d7bc8fc8a78e314950
mcdonmic <pdx.mjm@gmail.com>
2015-01-25 03:18:44 -0800

    Initial Commit
    Loading Fun Facts into GitHub

--- app/src/main/java/info/sparq/funfacts/FunFactsActivity.java
+++ app/src/main/java/info/sparq/funfacts/FunFactsActivity.java
@@ -1,12 +1,23 @@
 package info.sparq.funfacts;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class FunFactsActivity extends ActionBarActivity {
+public class FunFactsActivity extends Activity {
+
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel freshColor = new ColorWheel();
+
+    /*
+    The next method is created by the IDE.  I sure wish I knew what setContentView() meant.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,26 +25,4 @@ public class FunFactsActivity extends ActionBarActivity {
         setContentView(R.layout.activity_fun_facts);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fun_facts, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
